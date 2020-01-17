35b04886c11cb30a2fd682dc49923d2459887c42
RicardoAguiar <rcag82@gmail.com>
2020-01-17 18:11:51 +0000

    Registering blit tests tests

--- AmplifyBloomHDR/Resources/BloomHDR.shader
+++ AmplifyBloomHDR/Resources/BloomHDR.shader
@@ -1,21 +1,14 @@
 Shader "Hidden/AmplifyBloomHDR"
 {
-    SubShader
-    {
+	SubShader
+	{
 		Cull Off ZWrite Off ZTest Always
 		HLSLINCLUDE
 			#pragma target 3.0
 			#include "Packages/com.unity.render-pipelines.core/ShaderLibrary/Common.hlsl"
 			#include "Packages/com.unity.render-pipelines.core/ShaderLibrary/Color.hlsl"
 			#include "Packages/com.unity.render-pipelines.high-definition/Runtime/ShaderLibrary/ShaderVariables.hlsl"
-
-			TEXTURE2D_X (_MainTex);
-			float4 _MainTex_TexelSize;// x - 1/width y - 1/height z- width w - height
-
-			#define USING_HDRP
-			#include "../../AmplifyBloom/Resources/BloomLib.cginc"
-			#include "../../AmplifyBloom/Resources/BloomSRPTools.hlsl"
-
+			TEXTURE2D( _MainTex );
 		ENDHLSL
 
 		Pass
@@ -55,9 +48,51 @@ Shader "Hidden/AmplifyBloomHDR"
 			{
 				UNITY_SETUP_STEREO_EYE_INDEX_POST_VERTEX (input);
 				uint2 positionSS = input.texcoord * _ScreenSize.xy;
-				float4 c1 = ASESampleTex(_MainTex, positionSS);
-				
-				return c1 * _Color;
+				float4 c1 = LOAD_TEXTURE2D (_MainTex, positionSS);
+				return c1*_Color;
+			}
+			ENDHLSL
+		}
+
+		Pass
+		{
+			HLSLPROGRAM
+
+			#pragma vertex Vertex
+			#pragma fragment Fragment
+
+			struct Attributes
+			{
+				uint vertexID : SV_VertexID;
+				UNITY_VERTEX_INPUT_INSTANCE_ID
+			};
+
+			struct Varyings
+			{
+				float4 positionCS : SV_POSITION;
+				float2 texcoord   : TEXCOORD0;
+				UNITY_VERTEX_OUTPUT_STEREO
+			};
+
+
+			float4 _Color;
+
+			Varyings Vertex (Attributes input)
+			{
+				Varyings output;
+				UNITY_SETUP_INSTANCE_ID (input);
+				UNITY_INITIALIZE_VERTEX_OUTPUT_STEREO (output);
+				output.positionCS = GetFullScreenTriangleVertexPosition (input.vertexID);
+				output.texcoord = GetFullScreenTriangleTexCoord (input.vertexID);
+				return output;
+			}
+
+			float4 Fragment (Varyings input) : SV_Target
+			{
+				UNITY_SETUP_STEREO_EYE_INDEX_POST_VERTEX (input);
+				uint2 positionSS = input.texcoord * _ScreenSize.xy;
+				float4 c1 = LOAD_TEXTURE2D (_MainTex, positionSS);
+				return c1;
 			}
 			ENDHLSL
 		}
--- AmplifyBloomHDR/Runtime/AmplifyBloomHDR.cs
+++ AmplifyBloomHDR/Runtime/AmplifyBloomHDR.cs
@@ -13,11 +13,11 @@ namespace AmplifyBloom
 		static class ShaderIDs
 		{
 			internal static readonly int Color = Shader.PropertyToID( "_Color" );
-			internal static readonly int InputTexture = Shader.PropertyToID( "_InputTexture" );
+			internal static readonly int InputTexture = Shader.PropertyToID( "_MainTex" );
 		}
 
 		Material _material;
-		
+
 		public bool IsActive() => _material != null ;
 
 		public override CustomPostProcessInjectionPoint injectionPoint => CustomPostProcessInjectionPoint.AfterPostProcess;
@@ -27,12 +27,51 @@ namespace AmplifyBloom
 			_material = CoreUtils.CreateEngineMaterial( "Hidden/AmplifyBloomHDR" );
 		}
 
+		
 		public override void Render( CommandBuffer cmd, HDCamera camera, RTHandle srcRT, RTHandle destRT )
 		{
-			int passID = 0;
-			_material.SetColor( ShaderIDs.Color, color.value );
-			_material.SetTexture( ShaderIDs.InputTexture, srcRT );
-			HDUtils.DrawFullScreen( cmd, _material, destRT, null, passID );
+			//int passID = 0;
+			//_material.SetColor( ShaderIDs.Color, color.value );
+			//_material.SetTexture( ShaderIDs.InputTexture,srcRT );
+
+			//cmd.SetGlobalTexture( ShaderIDs.InputTexture, srcRT );
+			//cmd.SetGlobalColor( ShaderIDs.Color, color.value );
+
+			int nameId = Shader.PropertyToID( "_NomeAleatorio" );
+			int nameId2 = Shader.PropertyToID( "_NomeAleatorio2" );
+			cmd.GetTemporaryRT( nameId, camera.actualWidth, camera.actualHeight, 0, FilterMode.Point, srcRT.rt.graphicsFormat );
+			cmd.GetTemporaryRT( nameId2, camera.actualWidth, camera.actualHeight, 0, FilterMode.Point, srcRT.rt.graphicsFormat );
+
+			//cmd.SetGlobalTexture( ShaderIDs.InputTexture, srcRT );
+			//cmd.Blit( srcRT, nameId);
+
+			//cmd.SetGlobalTexture( ShaderIDs.InputTexture, nameId );
+			//cmd.Blit( srcRT, destRT );
+
+			cmd.CopyTexture( srcRT, 0, 0, nameId, 0, 0 );
+
+			cmd.SetGlobalTexture( ShaderIDs.InputTexture, nameId );
+			cmd.Blit( nameId, nameId2, _material, 0 );
+
+			//Bloom Code goes here
+
+			cmd.CopyTexture( nameId2, 0, 0, destRT, 0, 0 );
+
+			//cmd.SetGlobalTexture( ShaderIDs.InputTexture, nameId );
+			//RTHandles.Alloc(
+			//HDUtils.DrawFullScreen( cmd, _material, nameId, null, 0 );
+			//
+			//cmd.SetGlobalTexture( ShaderIDs.InputTexture, nameId );
+			//HDUtils.DrawFullScreen( cmd, _material, destRT, null, 1 );
+
+
+			cmd.ReleaseTemporaryRT( nameId );
+			cmd.ReleaseTemporaryRT( nameId2 );
+
+			//cmd.SetGlobalTexture( ShaderIDs.InputTexture, nameId );
+			//HDUtils.DrawFullScreen( cmd, _material, destRT, null, 1 );
+			//HDUtils.DrawFullScreen( cmd, _material, destRT, null, 0 );
+
 		}
 
 		public override void Cleanup()
